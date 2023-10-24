2ea8298cd2854d426c4d5af8902c881ca6e3632b
shushengyuan <2637975471@qq.com>
2023-10-24 20:26:43 +0800

    log: real original topk.cu

--- src/old.cu
+++ src/old.cu
@@ -3,15 +3,10 @@
 
 typedef uint4 group_t;  // uint32_t
 
-// 定义一个常量，表示每个线程束的大小
-#define WARP_SIZE 32
-
-// 定义一个函数模板，用于指定块的大小
-template <unsigned int blockSize>
-__global__ void docQueryScoringCoalescedMemoryAccessSampleKernelNew(
+void __global__ docQueryScoringCoalescedMemoryAccessSampleKernel(
     const __restrict__ uint16_t *docs, const int *doc_lens, const size_t n_docs,
     uint16_t *query, const int query_len, float *scores) {
-  // 每个线程处理一个文档-查询对的评分任务
+  // each thread process one doc-query pair scoring task
   register auto tid = blockIdx.x * blockDim.x + threadIdx.x,
                 tnum = gridDim.x * blockDim.x;
 
@@ -20,10 +15,10 @@ __global__ void docQueryScoringCoalescedMemoryAccessSampleKernelNew(
   }
 
   __shared__ uint16_t query_on_shm[MAX_QUERY_SIZE];
-  // 使用循环展开来减少分支冲突
 #pragma unroll
-  for (auto i = threadIdx.x; i < query_len; i += blockSize) {
-    query_on_shm[i] = query[i];  // 不太高效的查询加载，假设它不是热点
+  for (auto i = threadIdx.x; i < query_len; i += blockDim.x) {
+    query_on_shm[i] = query[i];  // not very efficient query loading temporally,
+                                 // as assuming its not hotspot
   }
 
   __syncthreads();
@@ -40,7 +35,6 @@ __global__ void docQueryScoringCoalescedMemoryAccessSampleKernelNew(
       if (no_more_load) {
         break;
       }
-      // 使用分组内存访问来提高内存吞吐量
       register group_t loaded = ((group_t *)docs)[i * n_docs + doc_id];  // tid
       register uint16_t *doc_segment = (uint16_t *)(&loaded);
       for (auto j = 0; j < sizeof(group_t) / sizeof(uint16_t); j++) {
@@ -49,7 +43,6 @@ __global__ void docQueryScoringCoalescedMemoryAccessSampleKernelNew(
           break;
           // return;
         }
-        // 使用位运算代替求余运算
         while (query_idx < query_len &&
                query_on_shm[query_idx] < doc_segment[j]) {
           ++query_idx;
@@ -58,12 +51,12 @@ __global__ void docQueryScoringCoalescedMemoryAccessSampleKernelNew(
           tmp_score += (query_on_shm[query_idx] == doc_segment[j]);
         }
       }
-      // 使用__syncwarp()代替__syncthreads()来同步分组中的线程
       __syncwarp();
     }
     scores[doc_id] = tmp_score / max(query_len, doc_lens[doc_id]);  // tid
   }
 }
+
 void doc_query_scoring_gpu_function(
     std::vector<std::vector<uint16_t>> &querys,
     std::vector<std::vector<uint16_t>> &docs, std::vector<uint16_t> &lens,
