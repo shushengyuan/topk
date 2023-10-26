1a651c5909899c7cda10d29c076f5c43c7a9fc96
shushengyuan <2637975471@qq.com>
2023-10-26 21:17:48 +0800

    feat: minor optimization

--- src/topk.h
+++ src/topk.h
@@ -9,6 +9,7 @@
 #define MAX_QUERY_SIZE 4096
 #define N_THREADS_IN_ONE_BLOCK 512
 #define TOPK 100
+#define Group_Size 8
 
 void doc_query_scoring_gpu_function(std::vector<std::vector<uint16_t>> &query,
                                     std::vector<std::vector<uint16_t>> &docs,
