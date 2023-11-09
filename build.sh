c8358878b47d328428cc522951b10873a80abe0d
shushengyuan <2637975471@qq.com>
2023-11-09 17:08:56 +0800

    feat: add sub::sort and remove omp

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ ROOT_DIR=$1
 if [ -z "$ROOT_DIR" ]; then
         ROOT_DIR=$(cd $(dirname $0); pwd)
 fi
-
-nvcc $ROOT_DIR/src/main.cpp  $ROOT_DIR/src/topk.cu -o $ROOT_DIR/bin/query_doc_scoring  -I $ROOT_DIR/src -L/usr/local/cuda/lib64 -lcudart -lcuda -Xcompiler -fopenmp  -O4
+#  -Xcompiler -fopenmp 
+nvcc $ROOT_DIR/src/main.cpp  $ROOT_DIR/src/topk.cu -o $ROOT_DIR/bin/query_doc_scoring  -I $ROOT_DIR/src -L/usr/local/cuda/lib64 -lcudart -lcuda -O4
 echo "build success"
