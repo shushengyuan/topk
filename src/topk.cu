3f6137a815764d82fc2863d10f9f17fa6b2ddb97
shushengyuan <2637975471@qq.com>
2023-11-12 16:56:44 +0800

    feat: add batch size

--- src/topk.cu
+++ src/topk.cu
@@ -22,20 +22,19 @@ typedef uint4 group_t;  // uint32_t
 
 void __global__ docQueryScoringCoalescedMemoryAccessSampleKernel(
     const __restrict__ uint16_t *docs, const uint16_t *doc_lens,
-    const size_t n_docs, uint16_t *query, const int total_querys_len,
+    const size_t n_docs, uint16_t *query, const int bach_now,
     const uint16_t *query_lens_d, float *scores, int *s_indices) {
   // each thread process one doc-query pair scoring task
   register auto tidx = blockIdx.x * blockDim.x + threadIdx.x,
                 tnumx = gridDim.x * blockDim.x;
 
-  if (tidx >= n_docs * total_querys_len) {
+  if (tidx >= n_docs * bach_now) {
     return;
   }
   __shared__ uint32_t query_on_shm[MAX_QUERY_SIZE];
   // __shared__ uint32_t doc_lens_on_shm[n_docs];
 
-  for (auto doc_id = tidx; doc_id < n_docs * total_querys_len;
-       doc_id += tnumx) {
+  for (auto doc_id = tidx; doc_id < n_docs * bach_now; doc_id += tnumx) {
     register int query_idx = 0;
 
     register float tmp_score = 0.;
@@ -141,9 +140,10 @@ void doc_query_scoring_gpu_function(
 
   size_t n_docs = docs.size();
   int total_querys_len = querys.size();
+  const int BATCH_SIZE = total_querys_len / 2;
 
   int block = N_THREADS_IN_ONE_BLOCK;
-  int grid = ((total_querys_len * n_docs) + block - 1) / block;
+  int grid = ((BATCH_SIZE * n_docs) + block - 1) / block;
 
   uint16_t *d_docs = nullptr;
   uint16_t *d_doc_lens = nullptr;
@@ -195,64 +195,83 @@ void doc_query_scoring_gpu_function(
       << "init cost "
       << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count()
       << " ms " << std::endl;
-  // init indices
-  nvtxRangePushA("query start");
-  uint16_t *d_query = nullptr;
-  float *d_scores = nullptr;
-  int *s_indices = nullptr;
-  uint16_t *query_lens_d = nullptr;
+  t_query.join();
 
-  cudaMallocAsync(&d_query,
-                  sizeof(uint16_t) * MAX_QUERY_SIZE * total_querys_len,
-                  streams[0]);
+  uint16_t *query_lens_d = nullptr;
   cudaMallocAsync(&query_lens_d, sizeof(uint16_t) * total_querys_len,
                   streams[1]);
-  t_query.join();
-  cudaMemcpyAsync(d_query, h_query,
-                  sizeof(uint16_t) * MAX_QUERY_SIZE * total_querys_len,
-                  cudaMemcpyHostToDevice, streams[0]);
   cudaMemcpyAsync(query_lens_d, query_lens_vec.data(),
                   sizeof(uint16_t) * total_querys_len, cudaMemcpyHostToDevice,
                   streams[1]);
-
-  cudaMallocAsync(&d_scores, sizeof(float) * n_docs * total_querys_len,
-                  streams[2]);
-  cudaMallocAsync(&s_indices, sizeof(int) * n_docs * total_querys_len,
-                  streams[3]);
   cudaStreamSynchronize(streams[1]);
-  cudaStreamSynchronize(streams[2]);
-  cudaStreamSynchronize(streams[3]);
-  docQueryScoringCoalescedMemoryAccessSampleKernel<<<grid, block, 0,
-                                                     streams[0]>>>(
-      d_docs, d_doc_lens, n_docs, d_query, total_querys_len, query_lens_d,
-      d_scores, s_indices);
+  auto i = 0;
 
-  nvtxRangePop();
-#pragma unroll
-  for (int i = 0; i < total_querys_len; ++i) {
-    nvtxRangePushA("sort_by_key");
-    void *d_temp_storage = nullptr;
-    size_t temp_storage_bytes = 0;
-    int *d_sort_index = nullptr;
-    float *d_sort_scores = nullptr;
+  while (i < total_querys_len) {
+    auto bach_now = BATCH_SIZE;
+    if (i + BATCH_SIZE == total_querys_len) {
+      bach_now = BATCH_SIZE - 1;
+      // break;
+    } else if (i + BATCH_SIZE > total_querys_len) {
+      bach_now = total_querys_len - i - 1;
+    }
+    if (bach_now == 0) {
+      break;
+    }
 
-    cudaMallocAsync(&d_sort_index, sizeof(int) * n_docs, streams[i]);
-    cudaMallocAsync(&d_sort_scores, sizeof(float) * n_docs, streams[i]);
+    nvtxRangePushA("query start");
 
-    cub::DeviceRadixSort::SortPairsDescending(
-        d_temp_storage, temp_storage_bytes, d_scores + i * n_docs,
-        d_sort_scores, s_indices + i * n_docs, d_sort_index, n_docs);
-    // Allocate temporary storage
-    cudaMalloc(&d_temp_storage, temp_storage_bytes);
-    // cudaDeviceSynchronize();
+    float *d_scores = nullptr;
+    int *s_indices = nullptr;
+    uint16_t *d_query = nullptr;
 
-    cub::DeviceRadixSort::SortPairsDescending(
-        d_temp_storage, temp_storage_bytes, d_scores + i * n_docs,
-        d_sort_scores, s_indices + i * n_docs, d_sort_index, n_docs);
+    cudaMallocAsync(&d_query, sizeof(uint16_t) * MAX_QUERY_SIZE * bach_now,
+                    streams[i]);
 
-    cudaMemcpyAsync(indices_pre[i].data(), d_sort_index, sizeof(int) * TOPK,
-                    cudaMemcpyDeviceToHost, streams[i]);
+    cudaMemcpyAsync(d_query, h_query + i * MAX_QUERY_SIZE,
+                    sizeof(uint16_t) * MAX_QUERY_SIZE * bach_now,
+                    cudaMemcpyHostToDevice, streams[i]);
+
+    cudaMallocAsync(&d_scores, sizeof(float) * n_docs * bach_now, streams[2]);
+    cudaMallocAsync(&s_indices, sizeof(int) * n_docs * bach_now, streams[3]);
+    docQueryScoringCoalescedMemoryAccessSampleKernel<<<grid, block, 0,
+                                                       streams[i]>>>(
+        d_docs, d_doc_lens, n_docs, d_query, bach_now, query_lens_d + i,
+        d_scores, s_indices);
+
     nvtxRangePop();
+#pragma unroll
+    for (int j = 0; j < bach_now; ++j) {
+      nvtxRangePushA("sort_by_key");
+      void *d_temp_storage = nullptr;
+      size_t temp_storage_bytes = 0;
+      int *d_sort_index = nullptr;
+      float *d_sort_scores = nullptr;
+
+      cudaMallocAsync(&d_sort_index, sizeof(int) * n_docs, streams[j]);
+      cudaMallocAsync(&d_sort_scores, sizeof(float) * n_docs, streams[j]);
+
+      cub::DeviceRadixSort::SortPairsDescending(
+          d_temp_storage, temp_storage_bytes, d_scores + j * n_docs,
+          d_sort_scores, s_indices + j * n_docs, d_sort_index, n_docs);
+      // Allocate temporary storage
+      cudaMalloc(&d_temp_storage, temp_storage_bytes);
+      // cudaDeviceSynchronize();
+
+      cub::DeviceRadixSort::SortPairsDescending(
+          d_temp_storage, temp_storage_bytes, d_scores + j * n_docs,
+          d_sort_scores, s_indices + j * n_docs, d_sort_index, n_docs);
+
+      cudaMemcpyAsync(indices_pre[j + i].data(), d_sort_index,
+                      sizeof(int) * TOPK, cudaMemcpyDeviceToHost, streams[j]);
+      // cudaFreeAsync(d_sort_index, streams[j]);
+      // cudaFreeAsync(d_sort_scores, streams[j]);
+      nvtxRangePop();
+    }
+    cudaFreeAsync(s_indices, streams[i]);
+    // cudaFreeAsync(d_scores, streams[i]);
+    // cudaFreeAsync(d_query, streams[i]);
+
+    i += bach_now;
   }
 
   indices = indices_pre;
